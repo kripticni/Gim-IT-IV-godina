2ef943e91220729f1943c976693a2885a0fa997b
Lazar Aleksic <kripticni@proton.me>
2025-12-29 09:30:58 +0100

    root: dodati svi fajlovi za prvi javascript cas

---
+++ pravougaonik.js
@@ -0,0 +1,5 @@
+a=parseFloat(prompt("Unesi stranicu a pravougaonika: "));
+b=parseFloat(prompt("Unesi stranicu b pravougaonika: "));
+povrsina=a*b;
+obim=2*(a+b);
+alert("Obim je, " + obim + ", dok je povrsina " + povrsina);
---
+++ zbir.js
@@ -0,0 +1,4 @@
+x=parseInt(prompt("unesi nesto celobrojno"));
+y=parseFloat(prompt("unesi nesto realno"));
+sum=x+y;
+alert("Zbir brojeva X i Y je "+sum);
